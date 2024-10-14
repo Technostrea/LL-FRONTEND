f114663dd9311cb5805fa1f83eb5c8ed2930197c
benaja-bendo <benaja.bendo02@gmail.com>
2024-10-14 16:49:07 +0200

    add & config variable d'environement

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: import.meta.env.NG_APP_API_BASE_URL,
+  endpoints: {
+    getAllProperties: `/properties`,
+    getShowProperties: (id: string) => `/properties/${id}`,
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: import.meta.env.NG_APP_API_BASE_URL || 'http://localhost/api/v1',
+  endpoints: {
+    getAllProperties: `/properties`,
+    getShowProperties: (id: string) => `/properties/${id}`,
+  },
 };
