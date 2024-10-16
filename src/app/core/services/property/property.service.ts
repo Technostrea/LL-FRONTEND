f931a60f7c230d37e765163e5ba8ae5c5e2d7288
benaja-bendo <benaja.bendo02@gmail.com>
2024-10-16 14:09:41 +0200

    refactor endpoint modulaire

--- src/app/core/services/property/property.service.ts
+++ src/app/core/services/property/property.service.ts
@@ -1,26 +1,24 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { PropertyType } from '@app/shared/models/property-type.js';
-import { ResponeApiType } from '@app/shared/models/respone-api-type.js';
-import { environment } from '@env/environment.development';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {PropertyType} from '@app/shared/models/property-type.js';
+import {ResponeApiType} from '@app/shared/models/respone-api-type.js';
+import {environment} from '@env/environment.development';
+import {Observable} from 'rxjs';
+import {ApiService} from "@app/core/services/api.service";
 
 @Injectable({
   providedIn: 'root',
 })
 export class PropertyService {
-  private baseApi = environment.apiUrl;
-  constructor(private http: HttpClient) {}
+  private propertiesEndPoint = environment.endpoints.properties;
+
+  constructor(private http: ApiService) {
+  }
 
   getProperties(): Observable<ResponeApiType<PropertyType[]>> {
-    return this.http.get<ResponeApiType<PropertyType[]>>(
-      this.baseApi + environment.endpoints.getAllProperties
-    );
+    return this.http.get<ResponeApiType<PropertyType[]>>(this.propertiesEndPoint.GET_ALL_PROPERTY);
   }
 
   getShowProperty(id: string): Observable<ResponeApiType<PropertyType>> {
-    return this.http.get<ResponeApiType<PropertyType>>(
-      this.baseApi + environment.endpoints.getShowProperties(id)
-    );
+    return this.http.get<ResponeApiType<PropertyType>>(this.propertiesEndPoint.GET_SHOW_PROPERTY(id));
   }
 }
